801460cb76e5e07a15243a5e9534e05d23fc6526
Ujjwal Sharma <92476049+Ujjwal2421@users.noreply.github.com>
2024-02-17 23:51:57 +0530

    Update EditEquityForm.js

--- src/MyComponent/EditEquityForm.js
+++ src/MyComponent/EditEquityForm.js
@@ -1,9 +1,9 @@
-import React,{useEffect, useState} from 'react'
+import React,{useState} from 'react'
 import "./App2.css"
-import infom from './Info.json'
-const EditEquityForm = () => {
-   const[data,setData]=useState(''); 
-   const keys = Object.keys(infom); 
+import CrudServices from '../Services/CrudServices';
+
+const service=new CrudServices()
+const AddEquity = () => {
     const [formData, setFormData] = useState({
         field1: '',
         field2: '',
@@ -66,6 +66,7 @@ const EditEquityForm = () => {
         field59: '',
         field60: '',
         field61: '',
+        field62: '',
     });
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -74,102 +75,21 @@ const EditEquityForm = () => {
             [name]: value,
         }));
     };
-    const handleSubmit=(e)=>{
+    const handleClick=(e)=>{
         e.preventDefault()
-        console.log(formData)
-    }
-    const handleEditSubmit=(e)=>{
-        e.preventDefault()
-        alert(data);
-       setFormData({
-        field1: infom["Security Name"],
-        field2: infom['Security Description'],    
-        field3: infom['Has Position'],
-        field4: infom["Is Active Security"],
-        field5: infom['Lot Size'],
-        field6: infom["BBG Unique Name"],
-        field7: infom[ "CUSIP"],
-        field8: infom['ISIN'],
-        field9: infom["SEDOL"],
-        field10: infom['Bloomberg Ticker'],
-        field11: infom['Bloomberg Unique ID'],
-        field12: infom['BBG Global ID'],
-        field13: infom['Ticker and Exchange'],
-        field14: infom['Is ADR Flag'],
-        field15: infom['ADR Underlying Ticker'],
-        field16: infom['ADR Underlying Currency"'],
-        field17: infom['Shares Per ADR'],
-        field18: infom['IPO Date'],
-        field19: infom['Pricing Currency'],
-        field20: infom['Settle Days'],
-        field21: infom['Total Shares Outstanding'],
-        field22: infom['Voting Rights Per Share'],
-        field23: infom['Average Volume - 20D'],
-        field24: infom['Beta'],
-        field25: infom['Return - YTD'],
-        field26: infom['Volatility - 90D'],
-        field27: infom['PF Asset Class'],
-        field28: infom['PF Country'],
-        field29: infom['PF Credit Rating'],
-        field30: infom['PF Currency'],
-        field31: infom['PF Instrument'],
-        field32: infom['PF Liquidity Profile'],
-        field34: infom['PF Maturity'],
-        field35: infom['PF NAICS Code'],
-        field36: infom['PF Region'],
-        field37: infom["PF Region"],
-        field38: infom[ "PF Sector"],
-        field39: infom[  "PF Sub Asset Class"],
-        field40: infom["Country of Issuance"],
-        field41: infom["Exchange"],
-        field42: infom["Issue Currency"],
-        field43: infom["Trading Currency"],
-        field44: infom["BBG Industry Sub Group"],
-        field45: infom["Bloomberg Industry Group"],
-        field46: infom["Bloomberg Sector"],
-        field47: infom["Country of Incorporation"],
-        field48: infom["Risk Currency"],
-        field49: infom["Open Price"],
-        field50: infom[ "Close Price"],
-        field51: infom["Volume"],
-        field52: infom["Last Price"],
-        field53: infom["Ask Price"],
-        field54: infom[ "Bid Price"],
-        field55: infom["PE Ratio"],
-        field56: infom["Dividend Ex Date"],
-        field57: infom["Dividend Record Date"],
-        field58: infom[ "Dividend Pay Date"],
-        field59: infom["Dividend Amount"],
-        field60: infom["Frequency"],
-        field61: infom['Dividend Type'],
-       })
-        console.log(formData)
-    }
-
-    
-    // useEffect(() => {
         
-    //     axios.get('/Info.json')
-    //       .then(response => response.json())
-    //       .then(data => {
-    //         console.log(data)
-    //         const selectedItem = data.find(item => item.id ===location.state.info);
-    //         console.log(location.state.info)
-    //         console.log(selectedItem)
-    //       })
-    //       .catch(error => console.error('Error fetching data:', error));
-    //   }); 
+        service.createRecord(formData).then((formData)=>{
+          console.log(formData)
+        }).catch((error)=>{ console.log(error) })
+    }
+   
   return (
-    <div>   
-        <form className="d-flex" role="search" onSubmit={handleSubmit}>
-        <input className="form-control me-2" type="search" placeholder="Name of Equity" aria-label="Search" value={data} onChange={(e)=>setData(e.target.value)}/>
-        <button className="btn btn-outline-primary" type="submit" onClick={handleEditSubmit}>Edit</button>
-      </form><br/><br/>
+    <div>
                      <h1 className="centered-heading">Equity Security</h1>
                     <div className="form-container">
-                      <form  onSubmit={handleSubmit} className="form-group">
+                      <form   className="form-group">
                         <label>Security Name:</label>
-                        <input type="text" id="text" name="field1" class="right-align" value={formData.field1} onChange={handleInputChange}/><br/><br/>
+                        <input type="text" id="name" name="field1" class="right-align" value={formData.field1} onChange={handleInputChange}/><br/><br/>
                         <label >Security Description:</label>
                         <input type="text" id="text" name="field2" class="right-align" value={formData.field2} onChange={handleInputChange}/><br/><br/>
                         <label>Has Position:</label>
@@ -250,48 +170,49 @@ const EditEquityForm = () => {
                         <input type="text" id="text" name="field40" class="right-align" value={formData.field40} onChange={handleInputChange}/><br/><br/>
                         <label >Issuer:</label>
                         <input type="text" id="text" name="field41" class="right-align" value={formData.field41} onChange={handleInputChange}/><br/><br/>
-              
-                        <label>Trading Currency:</label>
+                        <label >Issue Currency:</label>
                         <input type="text" id="text" name="field42" class="right-align" value={formData.field42} onChange={handleInputChange}/><br/><br/>
-                        <label>Bloomberg Industry Sub Group:</label>
+                        <label>Trading Currency:</label>
                         <input type="text" id="text" name="field43" class="right-align" value={formData.field43} onChange={handleInputChange}/><br/><br/>
-                        <label>Bloomberg Industry Group:</label>
+                        <label>Bloomberg Industry Sub Group:</label>
                         <input type="text" id="text" name="field44" class="right-align" value={formData.field44} onChange={handleInputChange}/><br/><br/>
-                        <label>Bloomberg Industry Sector:</label>
+                        <label>Bloomberg Industry Group:</label>
                         <input type="text" id="text" name="field45" class="right-align" value={formData.field45} onChange={handleInputChange}/><br/><br/>
-                        <label>Country of Incorporation:</label>
+                        <label>Bloomberg Industry Sector:</label>
                         <input type="text" id="text" name="field46" class="right-align" value={formData.field46} onChange={handleInputChange}/><br/><br/>
+                        <label>Country of Incorporation:</label>
+                        <input type="text" id="text" name="field47" class="right-align" value={formData.field47} onChange={handleInputChange}/><br/><br/>
                         <label>Risk Currency:</label>
-                        <input type="text" id="text" name="field47" value={formData.field47} onChange={handleInputChange}/><br/><br/>
-                        <label>Open Price:</label>
                         <input type="text" id="text" name="field48" value={formData.field48} onChange={handleInputChange}/><br/><br/>
-                        <label >Close Price:</label>
+                        <label>Open Price:</label>
                         <input type="text" id="text" name="field49" value={formData.field49} onChange={handleInputChange}/><br/><br/>
-                        <label>Volume:</label>
+                        <label >Close Price:</label>
                         <input type="text" id="text" name="field50" value={formData.field50} onChange={handleInputChange}/><br/><br/>
-                        <label>Last Price:</label>
+                        <label>Volume:</label>
                         <input type="text" id="text" name="field51" value={formData.field51} onChange={handleInputChange}/><br/><br/>
-                        <label>Ask Price:</label>
+                        <label>Last Price:</label>
                         <input type="text" id="text" name="field52" value={formData.field52} onChange={handleInputChange}/><br/><br/>
-                        <label>Bid Price:</label>
+                        <label>Ask Price:</label>
                         <input type="text" id="text" name="field53" value={formData.field53} onChange={handleInputChange}/><br/><br/>
-                        <label>PE Ratio:</label>
+                        <label>Bid Price:</label>
                         <input type="text" id="text" name="field54" value={formData.field54} onChange={handleInputChange}/><br/><br/>
+                        <label>PE Ratio:</label>
+                        <input type="text" id="text" name="field55" value={formData.field55} onChange={handleInputChange}/><br/><br/>
                         <label>Declared Date:</label>
-                        <input type="date" id="datetime" name="field55" value={formData.field55} onChange={handleInputChange}/><br/><br/>
-                        <label>Ex Date:</label>
                         <input type="date" id="datetime" name="field56" value={formData.field56} onChange={handleInputChange}/><br/><br/>
-                        <label>Record Date:</label>
+                        <label>Ex Date:</label>
                         <input type="date" id="datetime" name="field57" value={formData.field57} onChange={handleInputChange}/><br/><br/>
-                        <label>Pay Date:</label>
+                        <label>Record Date:</label>
                         <input type="date" id="datetime" name="field58" value={formData.field58} onChange={handleInputChange}/><br/><br/>
+                        <label>Pay Date:</label>
+                        <input type="date" id="datetime" name="field59" value={formData.field59} onChange={handleInputChange}/><br/><br/>
                         <label >Amount:</label>
-                        <input type="text" id="text" name="field59" value={formData.field59} onChange={handleInputChange}/><br/><br/>
-                        <label>Frequency:</label>
                         <input type="text" id="text" name="field60" value={formData.field60} onChange={handleInputChange}/><br/><br/>
-                        <label>Dividend Type:</label>
+                        <label>Frequency:</label>
                         <input type="text" id="text" name="field61" value={formData.field61} onChange={handleInputChange}/><br/><br/>
-                        <button className="form-btn">Submit</button><br/><br/>
+                        <label>Dividend Type:</label>
+                        <input type="text" id="text" name="field62" value={formData.field62} onChange={handleInputChange}/><br/><br/>
+                        <button className="form-btn" onClick={handleClick}>Submit</button><br/><br/>
                         <button className="form-btn">Close</button>
                       </form>
                       </div>
@@ -299,4 +220,4 @@ const EditEquityForm = () => {
   )
 }
 
-export default EditEquityForm
+export default AddEquity
