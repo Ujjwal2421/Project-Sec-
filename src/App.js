fd9a9c1e6622a510b796d1cebc2cb9eed516f79d
Ujjwal Sharma <92476049+Ujjwal2421@users.noreply.github.com>
2024-02-17 22:32:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import AddBond from './MyComponent/AddBond';
+import AddEquity from './MyComponent/AddEquity';
 import AddSec from './MyComponent/AddSec';
-import Frame from './MyComponent/Frame';
+import FetchBond from './MyComponent/FetchBond';
+import FetchEquity from './MyComponent/FetchEquity';
 import Login from './MyComponent/Login';
 import {  Route, Routes } from "react-router-dom";
+import SecurityView from './MyComponent/SecurityView';
+import EditEquityForm from './MyComponent/EditEquityForm';
+import EditBondForm from './MyComponent/EditBondForm';
 
 function App() {
   return (
@@ -11,7 +17,13 @@ function App() {
         <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/addsec" element={<AddSec/>} />
-        <Route path="/frame" element={<Frame/>} />
+        <Route path="/equity" element={<AddEquity/>} />
+        <Route path="/bond" element={<AddBond/>} />
+        <Route path="/fetche" element={<FetchEquity/>} />
+        <Route path="/fetchb" element={<FetchBond/>} />
+        <Route path="/viewsec" element={<SecurityView/>} />
+        <Route path="/edite" element={<EditEquityForm/>} />
+        <Route path="/editb" element={<EditBondForm/>} />
       </Routes>
     </div>
   );
